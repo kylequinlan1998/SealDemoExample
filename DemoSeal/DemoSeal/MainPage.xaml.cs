e05c8b65e61c4ba5f319a4220e4f92b49709877b
kylequinlan1998 <47331852+kylequinlan1998@users.noreply.github.com>
2020-01-21 18:58:02 +0000

    Basic Example Project Commit

---
+++ DemoSeal/DemoSeal/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Research.SEAL;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace DemoSeal
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            //Ciphertext is correctly recognised however encryption parameters throws error.
+            Ciphertext cipher;
+            //Error gets thrown here.
+            EncryptionParameters parms = new EncryptionParameters(SchemeType.BFV);
+        }
+    }
+}
